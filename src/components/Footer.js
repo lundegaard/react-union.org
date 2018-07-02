6121e2b5c0d6b3fe62f71978855c8c4beea3f33a
aizerin <l.nuzky@gmail.com>
2018-07-02 12:20:02 +0200

    added versions info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import Link from 'gatsby-link';
+import versions from '../../content/versions.json';
 
-const Footer = () => <div>Copyright © 2018 Lundegaard</div>;
+const Footer = () => (
+	<div>
+		Copyright © 2018 Lundegaard
+		<Link to="/history">{versions.actual}</Link>
+	</div>
+);
 
 export default Footer;
