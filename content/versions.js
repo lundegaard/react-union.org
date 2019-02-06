0011be62e3da899244b02835e3f2084e863cff62
Tomáš Konrády <tomaskonrady@gmail.com>
2019-02-06 10:36:10 +0100

    history

--- content/versions.js
+++ content/versions.js
@@ -1,9 +1,9 @@
 export default {
-	current: 'v0.6',
+	current: 'v0.12.0',
 	history: [
-		// {
-		// 	title: 'v0.4',
-		// 	url: 'https://github.com/lundegaard/react-union',
-		// },
+		{
+			title: 'v0.11.0',
+			url: 'https://5bb64200b312742aab7bce72--react-union.netlify.com',
+		},
 	],
 };
